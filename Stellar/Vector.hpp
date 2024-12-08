31fb41f059c2be60b78bd2ab669e2533de7268bf
Mez0ry <mez0ry@mail.ru>
2024-12-08 11:48:20 +0100

    Added second template paratemer for scalar

--- Stellar/Vector.hpp
+++ Stellar/Vector.hpp
@@ -9,39 +9,41 @@
 
 #include "Logger.hpp"
 
-template <typename _Ty>
-using enable_if_fundamental_t  = std::enable_if_t<std::is_fundamental_v<_Ty>>;
+template <typename T>
+using enable_if_fundamental_t  = std::enable_if_t<std::is_fundamental_v<T>>;
 
 /**
- * @param _Ty type of vector components, int by default \n 
- * @warning if you using  type (_Ty) that not fundamental it wouldn't enable instantiation
+ * @param TValue type of vector components, int by default \n
+ * @param TScalar type of scalar which used in operations such as multiplying vec by scalar, by default TScalar has type of TValue \n 
+ * @warning if you using type (TValue/TScalar) that not fundamental it wouldn't enable instantiation
 */
-template <typename _Ty = int,class = enable_if_fundamental_t<_Ty>>
+template <typename TValue = int,typename TScalar = TValue,class = enable_if_fundamental_t<TValue>, class = enable_if_fundamental_t<TScalar> >
 struct Vec2 {
-  _Ty x, y;
+  TValue x, y;
 
   Vec2() = default;
-  Vec2(_Ty pos_x, _Ty pos_y) : x(pos_x), y(pos_y) {}
+  Vec2(TValue pos_x, TValue pos_y) : x(pos_x), y(pos_y) {}
   Vec2(Vec2&& other) noexcept{
     swap(*this,other);
     other.clear();
   }
 
-  Vec2(Vec2<_Ty> &other) {
+  Vec2(Vec2<TValue> &other) {
     *this = other;
   }
   
-  Vec2(const Vec2<_Ty> &other) {
+  Vec2(const Vec2<TValue> &other) {
     (*this).x = other.x;
     (*this).y = other.y;
   }
 
   ~Vec2() = default;
 
-  friend void swap(Vec2<_Ty> &lhs, Vec2<_Ty> &rhs) {
+  friend void swap(Vec2<TValue> &lhs, Vec2<TValue> &rhs) {
     std::swap(lhs.x, rhs.x);
     std::swap(lhs.y, rhs.y);
   }
+
   /**
    * @brief used to get magnitude of the vector
    * @return magnitude
@@ -62,24 +64,24 @@ struct Vec2 {
    * @brief used to get distance
    * @return distance
    */
-  static _Ty Distance(const Vec2<_Ty> &lhs, const Vec2<_Ty> &rhs) {
-    Vec2<_Ty> res{std::abs(lhs.x - rhs.x), std::abs(lhs.y - rhs.y)};
+  static TValue Distance(const Vec2<TValue> &lhs, const Vec2<TValue> &rhs) {
+    Vec2<TValue> res{std::abs(lhs.x - rhs.x), std::abs(lhs.y - rhs.y)};
     return res.Length();
   }
 
   /**
    * @brief Get the maximum number of 2 components
    * 
-   * @return max component of type '_Ty'
+   * @return max component of type 'TValue'
   */
-  _Ty GetMaxComponent() const { return std::max(x, y); }
+  TValue GetMaxComponent() const { return std::max(x, y); }
 
   /**
    * @brief Get the minimum number of 2 components
    * 
-   * @return min component of type '_Ty'
+   * @return min component of type 'TValue'
   */
-  _Ty GetMinComponent() const { return std::min(x, y); }
+  TValue GetMinComponent() const { return std::min(x, y); }
 
   /**
    * @brief Get the angle of rotation
@@ -88,8 +90,8 @@ struct Vec2 {
    * @param rhs target vector
    * @return angle in radians
    */
-  static double GetRotationAngle(const Vec2<_Ty> &lhs, const Vec2<_Ty> &rhs) {
-    Vec2<_Ty> target_positive_perpendicular = (rhs - lhs).PositivePerpendicular();
+  static double GetRotationAngle(const Vec2<TValue> &lhs, const Vec2<TValue> &rhs) {
+    Vec2<TValue> target_positive_perpendicular = (rhs - lhs).PositivePerpendicular();
     return ( atan2(target_positive_perpendicular.y, target_positive_perpendicular.x));
   }
 
@@ -107,61 +109,61 @@ struct Vec2 {
     return cpy;
   }
 
-  Vec2<_Ty> PositivePerpendicular() const { return {-y, x}; }
+  Vec2<TValue> PositivePerpendicular() const { return {-y, x}; }
 
-  Vec2<_Ty> NegativePerpendicular() const { return {y, -x}; }
+  Vec2<TValue> NegativePerpendicular() const { return {y, -x}; }
 
   /**
    * @brief used to get dot product
    * @return dot product
    */
-  static double DotProduct(const Vec2<_Ty> &lhs, const Vec2<_Ty> &rhs) {
+  static double DotProduct(const Vec2<TValue> &lhs, const Vec2<TValue> &rhs) {
     return (lhs.x * rhs.x + lhs.y * rhs.y);
   }
 
-  static Vec2<_Ty> Lerp(const Vec2<_Ty> &lhs, const Vec2<_Ty> &rhs, float t) {
+  static Vec2<TValue> Lerp(const Vec2<TValue> &lhs, const Vec2<TValue> &rhs, float t) {
     t = std::clamp(t, 0.0f, 1.0f);
-    Vec2<_Ty> res;
+    Vec2<TValue> res;
     res.x = lhs.x + (rhs.x - lhs.x) * t;
     res.y = lhs.y + (rhs.y - lhs.y) * t;
     return res;
   }
 
   /** Comparison operators*/
-  Vec2<_Ty> operator-(const Vec2<_Ty> &rhs) const {
+  Vec2<TValue> operator-(const Vec2<TValue> &rhs) const {
     return {this->x - rhs.x, this->y - rhs.y};
   }
 
-  Vec2<_Ty> operator-(Vec2<_Ty> &rhs) const {
+  Vec2<TValue> operator-(Vec2<TValue> &rhs) const {
     return {this->x - rhs.x, this->y - rhs.y};
   }
 
-  Vec2<_Ty> operator+(const Vec2<_Ty> &rhs) const {
+  Vec2<TValue> operator+(const Vec2<TValue> &rhs) const {
     return {this->x + rhs.x, this->y + rhs.y};
   }
 
-  Vec2<_Ty> &operator+=(const Vec2<_Ty> &rhs) {
+  Vec2<TValue> &operator+=(const Vec2<TValue> &rhs) {
     this->x += rhs.x;
     this->y += rhs.y;
     return *this;
   }
 
-  Vec2<_Ty> &operator-=(const Vec2<_Ty> &rhs) {
+  Vec2<TValue> &operator-=(const Vec2<TValue> &rhs) {
     this->x -= rhs.x;
     this->y -= this->y - rhs.y;
     return *this;
   }
 
-  inline friend std::ostream &operator<<(std::ostream &out, const Vec2<_Ty> &vec) {
+  inline friend std::ostream &operator<<(std::ostream &out, const Vec2<TValue> &vec) {
     out << "Vector(" << vec.x << "," << vec.y << ")";
     return out;
   }
 
-  bool operator==(const Vec2<_Ty> &rhs) const {
+  bool operator==(const Vec2<TValue> &rhs) const {
     return (this->x == rhs.x && this->y == rhs.y);
   }
 
-  bool operator!=(const Vec2<_Ty> &rhs) const {
+  bool operator!=(const Vec2<TValue> &rhs) const {
     return !(*this == rhs);
   }
 
@@ -170,59 +172,61 @@ struct Vec2 {
   }
   
   /** Assignment operators*/
-  Vec2<_Ty> &operator=(const Vec2<_Ty>& other) {
+  Vec2<TValue> &operator=(const Vec2<TValue>& other) {
     if (this == &other) { // self assignment
       return (*this);
     }
+
     CopyAndSwap(*this,other);
+
     return (*this);
   }
 
-  Vec2<_Ty> operator+=(const float scalar) {
+  Vec2<TValue> operator+=(const TScalar scalar) {
     this->x += scalar;
     this->y += scalar;
     return *this;
   }
 
-  Vec2<_Ty> operator-=(const float scalar) {
+  Vec2<TValue> operator-=(const TScalar scalar) {
     this->x -= scalar;
     this->y -= scalar;
     return *this;
   }
 
-  Vec2<_Ty> operator*=(const float scalar) {
+  Vec2<TValue> operator*=(const TScalar scalar) {
     this->x *= scalar;
     this->y *= scalar;
     return *this;
   }
 
-  Vec2<_Ty> operator/=(const float scalar) {
+  Vec2<TValue> operator/=(const TScalar scalar) {
     this->x /= scalar;
     this->y /= scalar;
     return *this;
   }
 
-  inline Vec2<_Ty> operator+(const float scalar) {
-    Vec2<_Ty> tmp = (*this);
-    return tmp += (int)scalar;
+  inline Vec2<TValue> operator+(const TScalar scalar) {
+    Vec2<TValue> tmp = (*this);
+    return tmp += scalar;
   }
 
-  inline Vec2<_Ty> operator-(const float scalar) {
-    Vec2<_Ty> tmp = (*this);
-    return tmp -= (int)scalar;
+  inline Vec2<TValue> operator-(const TScalar scalar) {
+    Vec2<TValue> tmp = (*this);
+    return tmp -= scalar;
   }
 
-  inline Vec2<_Ty> operator*(const float scalar) {
-    Vec2<_Ty> tmp = (*this);
-    return tmp *= (int)scalar;
+  inline Vec2<TValue> operator*(const TScalar scalar) {
+    Vec2<TValue> tmp = (*this);
+    return tmp *= scalar;
   }
 
-  inline Vec2<_Ty> operator*(const Vec2<_Ty> &vec) {
+  inline Vec2<TValue> operator*(const Vec2<TValue> &vec) {
     return {(*this).x * vec.x, (*this).y * vec.y};
   }
 
-  inline Vec2<_Ty> operator/(const float scalar) {
-    Vec2<_Ty> tmp = (*this);
+  inline Vec2<TValue> operator/(const TScalar scalar) {
+    Vec2<TValue> tmp = (*this);
     return tmp /= scalar;
   }
 
@@ -233,8 +237,8 @@ struct Vec2 {
     this->y = 0;
   }
 
-  void CopyAndSwap(Vec2<_Ty>& l, const Vec2<_Ty>& other){
-    Vec2<_Ty> tmp(other);
+  void CopyAndSwap(Vec2<TValue>& l, const Vec2<TValue>& other){
+    Vec2<TValue> tmp(other);
     swap(l,tmp);
   }
 };
@@ -251,15 +255,15 @@ using Pointi = Vec2i;
 using Pointf = Vec2f;
 using Pointd = Vec2d;
 
-template <typename _Ty>
-struct fmt::formatter<Vec2<_Ty>> {
+template <typename TValue>
+struct fmt::formatter<Vec2<TValue>> {
     constexpr auto parse(format_parse_context& ctx) {
         return ctx.end();
     }
 
     template <typename Context>
-    auto format(const Vec2<_Ty>& p, Context& ctx) {
-      return format_to(ctx.out(), "Vector<{}>({}, {})", typeid(_Ty).name(),p.x, p.y);
+    auto format(const Vec2<TValue>& p, Context& ctx) {
+      return format_to(ctx.out(), "Vector<{}>({}, {})", typeid(TValue).name(),p.x, p.y);
     }
 };
 
@@ -279,8 +283,6 @@ template <> struct std::hash<Vec2<>> {
     return ((std::hash<int>()(k.x) ^ (std::hash<int>()(k.y) << 1)) >> 1);
   }
 };
- 
-
 
 #endif //!_VECTOR_HPP
 
