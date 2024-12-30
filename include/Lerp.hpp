1e6f37efc136063381b54e6ebe6b178926f4f361
Mez0ry <mez0ry@mail.ru>
2024-12-30 13:04:17 +0100

    implemented override for Lerp function to work with 'Color'

--- include/Lerp.hpp
+++ include/Lerp.hpp
@@ -1,11 +1,12 @@
 #ifndef _STELLAR_LERP_HPP__
 #define _STELLAR_LERP_HPP__
 #include <algorithm>
-
+class Color;
 namespace Stellar{
 
     float LerpUnClamped(float a, float b, float t);
     float Lerp(float a, float b, float t);
+    Color Lerp(Color start, Color target, float t);
 
 }//!Stellar
 
--- src/Lerp.cpp
+++ src/Lerp.cpp
@@ -1,5 +1,5 @@
 #include "Lerp.hpp"
-
+#include "Color.hpp"
 namespace Stellar
 {
     float LerpUnClamped(float a, float b, float t)
@@ -12,5 +12,15 @@ namespace Stellar
         auto t_clamped = std::clamp(0.0f,1.0f,t);
         return LerpUnClamped(a,b,t_clamped);
     }
-    
+
+    Color Lerp(Color start, Color target, float t)
+    {
+        Color res;
+        
+        res.r = Stellar::Lerp(start.r,target.r,t);
+        res.g = Stellar::Lerp(start.g,target.g,t);
+        res.b = Stellar::Lerp(start.b,target.b,t);
+        res.a = Stellar::Lerp(start.a,target.a,t);
+        return res;
+    }
 }
