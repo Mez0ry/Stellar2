53d4439275362a24865fe5ce16b2fdc9ae11ce92
Mez0ry <mez0ry@mail.ru>
2025-01-16 20:25:15 +0100

    Added package manager 'Conan'

---
+++ conanfile.py
@@ -0,0 +1,38 @@
+from conan import ConanFile
+from conan.tools.cmake.cmake import CMake
+from conan.tools.cmake import CMakeToolchain
+from conan.tools.build import check_max_cppstd, check_min_cppstd, default_cppstd
+from conan.tools.cmake import cmake_layout
+from conan.tools.files import copy
+
+class StellarLibrary(ConanFile):
+    name = "StellarLibrary"
+    version = "1.0"
+    settings = "os", "compiler", "arch", "build_type"
+    generators = "AutotoolsDeps","VirtualRunEnv", "VirtualBuildEnv","CMakeDeps", "CMakeToolchain"
+    
+    def validate(self):
+        default_cppstd(self,any,"-std=c++17")
+
+    def requirements(self):
+        self.requires("spdlog/1.15.0")
+        self.requires("sdl/2.30.9", override=True)
+        self.requires("sdl_image/2.8.2")
+        self.requires("sdl_ttf/2.22.0")
+        self.requires("sdl_mixer/2.8.0")
+        self.requires("fmt/11.0.2")
+        
+    def build_requirements(self):
+        self.tool_requires("cmake/3.31.3")
+        
+    def package_info(self):
+        self.cpp_info.cxxflags = ["-std=c++17"]
+        self.cpp_info.builddirs = ["pkg/cmake"]
+        self.cpp_info.set_property("cmake_find_mode", "none")
+    
+    def package(self):
+        copy(self, "*.so", self.build_folder, os.path.join(self.package_folder, "lib"))
+        copy(self, "*.o", self.build_folder, os.path.join(self.package_folder, "lib"))
+    
+    def layout(self):
+        cmake_layout(self)
